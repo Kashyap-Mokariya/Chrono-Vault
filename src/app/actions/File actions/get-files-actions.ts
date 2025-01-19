95598d4f708b5c50ef56518910e75f1e23b00d04
Kashyap Mokariya <mokariyakashyap@gmail.com>
2025-01-19 12:08:32 +0530

    Added dashboard page

--- src/app/actions/File actions/get-files-actions.ts
+++ src/app/actions/File actions/get-files-actions.ts
@@ -39,7 +39,7 @@ export const getFiles = async ({
                 .limit(limit);
         }
 
-        const {data, error} = await query
+        const { data, error } = await query
 
         if (error) {
             return {
@@ -54,3 +54,51 @@ export const getFiles = async ({
         handleError(error, "Failed to get files");
     }
 };
+
+export async function getTotalSpaceUsed() {
+
+    const supabase = await createClient()
+
+    try {
+
+        const { data: { user }} = await supabase.auth.getUser()
+
+        if (!user) throw new Error("User is not authenticated.");
+
+        const { data: files, error } = await supabase
+        .from("files")
+        .select('*')
+        .eq("user_id", user.id)
+
+        const totalSpace = {
+            image: { size: 0, latestDate: "" },
+            document: { size: 0, latestDate: "" },
+            video: { size: 0, latestDate: "" },
+            audio: { size: 0, latestDate: "" },
+            other: { size: 0, latestDate: "" },
+            used: 0,
+            all: 1 * 1024 * 1024 * 1024 /* 1GB available bucket storage */,
+        };
+
+        if(!files) {
+            return handleError(error, "Failed to get files")
+        }
+
+        files.forEach((file: SupabaseFile) => {
+            const fileType = file.type as FileType;
+            totalSpace[fileType].size += file.size;
+            totalSpace.used += file.size;
+
+            if (
+                !totalSpace[fileType].latestDate ||
+                new Date(file.updated_at) > new Date(totalSpace[fileType].latestDate)
+            ) {
+                totalSpace[fileType].latestDate = file.updated_at;
+            }
+        });
+
+        return parseStringify(totalSpace);
+    } catch (error) {
+        handleError(error, "Error calculating total space used:, ");
+    }
+}
